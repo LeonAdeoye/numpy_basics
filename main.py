0e08f7b9b71d3f9a21a429686d3e4ed334a05a76
LeonAdeoye <leon.adeoye@gmail.com>
2022-04-05 23:06:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy
+# NumPy is a Python library used for working with arrays.
+
+
+def start():
+    arr = numpy.array([1, 2, 3, 4, 5])
+    print(arr)
+
+
+if __name__ == '__main__':
+    start()
+
+
